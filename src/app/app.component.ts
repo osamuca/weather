34223c89fa22e64c40a252f9203adb2029c6f608
osamuca <sfverneck@gmail.com>
2017-12-22 11:10:28 -0200

    fix(lint): padrão lint de qualidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'cb-root',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cb';
+  title = 'cb'
 }
